9ff0fe1f9d385b12793c3bbe74b57ecc3264e270
sharvashish <srv@navstik.org>
2017-05-28 04:58:19 +0530

    changed logic of printing docker non-root how to?

--- linux/intelgraphics/openshell.sh
+++ linux/intelgraphics/openshell.sh
@@ -14,25 +14,24 @@ if [ ! $(command -v docker-compose) > /dev/null ]; then echo -e "${RED}ERROR${NC
 
 if ! 'groups' | grep -q docker
 	then
+	cat <<-EOF
+
+	If you would like to use Docker as a non-root user, you should now consider
+	adding your user to the "docker" group with something like:
+
+	  sudo usermod -aG docker $USER
+
+	Remember that you will have to log out and back in for this to take effect!
+
+	WARNING: Adding a user to the "docker" group will grant the ability to run
+	         containers which can be used to obtain root privileges on the
+	         docker host.
+	         Refer to https://docs.docker.com/engine/security/security/#docker-daemon-attack-surface
+	         for more information.
+
+	EOF
 	if [[ $EUID -ne 0 ]]; then
-		echo -e "${RED}ERROR${NC}: This script must be run as root, ${YLW}run with sudo ./start.sh, ${NC}exiting ...${NC}" 
-
-		cat <<-EOF
-
-		If you would like to use Docker as a non-root user, you should now consider
-		adding your user to the "docker" group with something like:
-
-		  sudo usermod -aG docker $USER
-
-		Remember that you will have to log out and back in for this to take effect!
-
-		WARNING: Adding a user to the "docker" group will grant the ability to run
-		         containers which can be used to obtain root privileges on the
-		         docker host.
-		         Refer to https://docs.docker.com/engine/security/security/#docker-daemon-attack-surface
-		         for more information.
-
-		EOF
+		echo -e "${RED}ERROR${NC}: This script must be run as root, unless you follow the above command, ${YLW}run with sudo ./start.sh, ${NC}exiting ...${NC}" 
 		exit 1
 	fi
 fi

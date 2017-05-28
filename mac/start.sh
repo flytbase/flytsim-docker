111a99c848612c2f8b09a200f34b75050b9ba043
sharvashish <srv@navstik.org>
2017-05-28 12:48:07 +0530

    commented out docker detection in mac start script

--- mac/start.sh
+++ mac/start.sh
@@ -8,36 +8,36 @@ NC='\033[0m' # No Color
 
 echo -e "${GRN}\nThis script is going to start FlytSim session for you\n${NC}"
 
-if ! 'groups' | grep -q docker
-	then
-	cat <<-EOF
+# if ! 'groups' | grep -q docker
+# 	then
+# 	cat <<-EOF
 
-	If you would like to use Docker as a non-root user, you should now consider
-	adding your user to the "docker" group with something like:
+# 	If you would like to use Docker as a non-root user, you should now consider
+# 	adding your user to the "docker" group with something like:
 
-	  sudo usermod -aG docker $USER
+# 	  sudo usermod -aG docker $USER
 
-	Remember that you will have to log out and back in for this to take effect!
+# 	Remember that you will have to log out and back in for this to take effect!
 
-	WARNING: Adding a user to the "docker" group will grant the ability to run
-	         containers which can be used to obtain root privileges on the
-	         docker host.
-	         Refer to https://docs.docker.com/engine/security/security/#docker-daemon-attack-surface
-	         for more information.
+# 	WARNING: Adding a user to the "docker" group will grant the ability to run
+# 	         containers which can be used to obtain root privileges on the
+# 	         docker host.
+# 	         Refer to https://docs.docker.com/engine/security/security/#docker-daemon-attack-surface
+# 	         for more information.
 
-	EOF
-	if [[ $EUID -ne 0 ]]; then
-		echo -e "${RED}ERROR${NC}: This script must be run as root, unless you follow the above command, ${YLW}run with sudo ./start.sh, ${NC}exiting ...${NC}" 
-		exit 1
-	fi
-fi
+# 	EOF
+# 	if [[ $EUID -ne 0 ]]; then
+# 		echo -e "${RED}ERROR${NC}: This script must be run as root, unless you follow the above command, ${YLW}run with sudo ./start.sh, ${NC}exiting ...${NC}" 
+# 		exit 1
+# 	fi
+# fi
 
-is_installed_and_running() {
-	echo -e "${YLW}Detecting if docker and docker-compose are already installed in this machine${NC}"
-	if [ ! $(command -v docker) > /dev/null ]; then echo -e "${RED}ERROR${NC}: docker does not seem to be installed. ${YLW}Please run ./setup.sh, ${NC}before running this script${NC}";exit 1;fi
-	if [ ! $(command -v docker-compose) > /dev/null ]; then echo -e "${RED}ERROR${NC}: docker-compose does not seem to be installed. ${YLW}Please run ./setup.sh, ${NC}before running this script${NC}";exit 1;fi
-	if ! pgrep dockerd > /dev/null; then echo -e "${RED}ERROR${NC}: docker does not seem to be running, has it been installed correctly, try rebooting your machine? ${YLW}Please run ./setup.sh, ${NC}before running this script${NC}";exit 1;fi
-}
+# is_installed_and_running() {
+# 	echo -e "${YLW}Detecting if docker and docker-compose are already installed in this machine${NC}"
+# 	if [ ! $(command -v docker) > /dev/null ]; then echo -e "${RED}ERROR${NC}: docker does not seem to be installed. ${YLW}Please run ./setup.sh, ${NC}before running this script${NC}";exit 1;fi
+# 	if [ ! $(command -v docker-compose) > /dev/null ]; then echo -e "${RED}ERROR${NC}: docker-compose does not seem to be installed. ${YLW}Please run ./setup.sh, ${NC}before running this script${NC}";exit 1;fi
+# 	if ! pgrep dockerd > /dev/null; then echo -e "${RED}ERROR${NC}: docker does not seem to be running, has it been installed correctly, try rebooting your machine? ${YLW}Please run ./setup.sh, ${NC}before running this script${NC}";exit 1;fi
+# }
 
 do_image_pull() {
 	cd $root_loc
@@ -120,7 +120,7 @@ docker_start() {
 }
 
 launch_flytsim() {
-	is_installed_and_running
+	#is_installed_and_running
 	do_image_pull
 	open_browser &
 	push_backup_files &
